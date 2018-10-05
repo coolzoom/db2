1886045e46276c50d005ab02f8367b76b9bac431
ejsvifq_mabmip <ejsvifq_mabmip@localhost>
2018-10-05 16:40:02 +0800

    cmt

---
+++ wdc2/modelfiledata.cc
@@ -0,0 +1,27 @@
+#include"modelfiledata.h"
+#include<array>
+#include<exception>
+#include<iostream>
+#include<fstream>
+#include"db2.h"
+#include<string>
+
+int main()
+try
+{
+	std::ifstream fin("modelfiledata.db2",std::ifstream::binary);
+	if(!fin)
+		fin.exceptions(std::ifstream::failbit);
+	std::string raw((std::istreambuf_iterator<char>(fin)),std::istreambuf_iterator<char>());
+	wdc2::db2<wdc2::modelfiledata> db2(raw);
+/*	for(const auto &ele : db2.field_storages)
+			std::cout<<ele<<'\n';
+	std::cout<<db2.sections.size()<<'\n';*/
+	for(const auto &ele : db2.sections.front().records)
+		std::cout<<ele<<'\n';
+}
+catch(const std::exception &e)
+{
+	std::cerr<<e.what()<<'\n';
+	return 1;
+}
--- wdc2/modelfiledata.h
+++ wdc2/modelfiledata.h
@@ -1,13 +1,40 @@
 #pragma once
 #include<cstdint>
 #include<array>
+#include"raw.h"
+#include"unknown.h"
 
 namespace wdc2
 {
 
-struct modelfiledata
+struct modelfiledata:unknown<5>{};
+
+std::uint32_t file_id(const modelfiledata& e)
 {
-	std::array<std::uint8_t,5> a;
-};
+	return get<std::uint32_t>(e,0,23);
+}
+
+std::uint32_t model_id(const modelfiledata& e)
+{
+	return get<std::uint16_t>(e,23,16);
+}
+
+void file_id(modelfiledata& e,std::uint32_t u)
+{
+	set(e,0,23,u);
+}
+
+void model_id(modelfiledata& e,std::uint16_t u)
+{
+	 set(e,23,16,u);
+}
+
+
+template<typename ostrm>
+inline decltype(auto) operator<<(ostrm& out,const modelfiledata& e)
+{
+	out<<"FILE ID:"<<file_id(e)<<"\tMODEL ID:"<<model_id(e);
+	return (out);
+}
 
 }
